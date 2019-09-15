0dd9f5fd5b7b3f56c1db261ebbfff4fd8e026d5d
unknown <pal18198@uvg.edu.gt>
2019-09-15 12:30:57 -0600

    Salta feo

--- Simulador3D/Assets/codigo/Main.cs
+++ Simulador3D/Assets/codigo/Main.cs
@@ -7,12 +7,19 @@ public class Main : MonoBehaviour
 {
     /************************************************* variables ***********************************************/
 
+    public double distancia = 0;
+
     //tabla
-    public double y1, x1;
-    public double y2, x2;
-    public double y3, x3;
-    public double y4, x4;
-    public double y5, x5;
+    public double y1;
+    public double y2;
+    public double y3;
+    public double y4;
+    public double y5;
+    public int x1;
+    public int x2;
+    public int x3;
+    public int x4;
+    public int x5;
 
     //carga, masa y velocidad inicial
     public double masa;
@@ -27,6 +34,10 @@ public class Main : MonoBehaviour
      double veFi;
 
     private Vector3 start;
+    private Vector3 holi;
+    private Vector3 holi1;
+    private Vector3 holi2;
+    private Vector3 holi3;
 
     // Start is called before the first frame update
     void Start()
@@ -43,7 +54,6 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int distancia = 0;
         float vel1 = (float)ve1;
         float vel2 = (float)ve2;
         float vel3 = (float)ve3;
@@ -51,28 +61,32 @@ public class Main : MonoBehaviour
         float vel5 = (float)veFi;
         if (distancia <= x1)
         {
-            transform.position = start + new Vector3(vel1, 0, 0);
-            distancia = distancia + 1;
+            transform.position = start + new Vector3(vel1 * Time.time, 0, 0);
+            distancia = distancia + 0.05;
+            holi = transform.position;
         }
         else if (distancia > x1 && distancia <= x2)
         {
-            transform.position = start + new Vector3(vel2, 0, 0);
-            distancia = distancia + 1;
+            transform.position = holi + new Vector3(vel2 * Time.time, 0, 0);
+            distancia = distancia + 0.05;
+            holi1 = transform.position;
         }
         else if (distancia > x2 && distancia <= x3)
         {
-            transform.position = start + new Vector3(vel3 * Time.deltaTime, 0, 0);
-            distancia = distancia + 1;
+            transform.position = holi1 + new Vector3(vel3 * Time.time, 0, 0);
+            distancia = distancia + 0.05;
+            holi2 = transform.position;
         }
         else if (distancia > x3 && distancia <= x4)
         {
-            transform.position = start + new Vector3(vel4 * Time.deltaTime, 0, 0);
-            distancia = distancia + 1;
+            transform.position = holi2 + new Vector3(vel4 * Time.time, 0, 0);
+            distancia = distancia + 0.05;
+            holi3 = transform.position;
         }
         else if (distancia > x4 && distancia <= x5)
         {
-            transform.position = start + new Vector3(vel5 * Time.time, 0, 0);
-            distancia = distancia + 1;
+            transform.position = holi3 + new Vector3(vel5 * Time.time, 0, 0);
+            distancia = distancia + 0.05;
         }
        /* else if (distancia > x5)
         {
