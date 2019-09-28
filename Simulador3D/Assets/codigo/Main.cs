6375efc3d4791818571f98657915fa6c77681fd0
unknown <pal18198@uvg.edu.gt>
2019-09-27 19:59:33 -0600

    falta la progra del menu

--- Simulador3D/Assets/codigo/Main.cs
+++ Simulador3D/Assets/codigo/Main.cs
@@ -9,19 +9,15 @@ public class Main : MonoBehaviour
 
  
     public double energiaI;
-    private double energiaIx;
-    private double energiaIy;
 
     //carga, masa y velocidad inicial
     public double masa;
     public double carga;
     public double veIn;
-    private Vector3 start;
 
     //resultados
     public double veFi;
     public float po1x;
-    public float po1y;
   
     
     public float time = 0.0f;
@@ -30,14 +26,22 @@ public class Main : MonoBehaviour
 
     float funcion(float x)
     {
-        return 0.01f*x*x;
+        return 0.01f *x*x;
+        //return 10f * x;
+        //return 0.01f * (float)Math.Sinh(x);
+        //return 100f * (float)Math.Sqrt(Math.Abs(x));
+        //return 0.01f * (float)Math.Cosh(x);
+        //return -10f * x;
+        //return 0.001f * x * x * x;
+        //return 10f*(float)Math.Abs(x);
+        //return 0.0001f*((float)Math.Pow(x, 6) * 0.5f)+(float)Math.Pow(x, 3) + (float)Math.Pow(x, 2) * 0.5f + (2*x) + 5f;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         // Posicion inicial de la particula
-        energiaI = (0.5 * masa * veIn * veIn) + (carga * 0.01*( x));
+        energiaI = (0.5 * masa * veIn * veIn) + (carga *funcion(x));
     }
 
     // Update is called once per frame
