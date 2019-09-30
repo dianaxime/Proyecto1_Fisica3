7bf68f4cdde56895911d78a9206e70870917ae76
unknown <pal18198@uvg.edu.gt>
2019-09-29 20:05:27 -0600

    casi el menu

--- Simulador3D/Assets/codigo/Main.cs
+++ Simulador3D/Assets/codigo/Main.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
     /************************************************* variables ***********************************************/
 
- 
+    public Menu menu;
     public double energiaI;
 
     //carga, masa y velocidad inicial
@@ -23,18 +24,33 @@ public class Main : MonoBehaviour
     public float time = 0.0f;
     float x = 0.0f;
     int sentido = 1;
+    int grafica ;
 
     float funcion(float x)
     {
-        return 0.01f *x*x;
-        //return 10f * x;
-        //return 0.01f * (float)Math.Sinh(x);
-        //return 100f * (float)Math.Sqrt(Math.Abs(x));
-        //return 0.01f * (float)Math.Cosh(x);
-        //return -10f * x;
-        //return 0.001f * x * x * x;
-        //return 10f*(float)Math.Abs(x);
-        //return 0.0001f*((float)Math.Pow(x, 6) * 0.5f)+(float)Math.Pow(x, 3) + (float)Math.Pow(x, 2) * 0.5f + (2*x) + 5f;
+        if (grafica == 1)
+        {
+            return 0.01f * x * x;
+        }
+        else if (grafica == 2)
+        {
+            return 10f * x;
+        }else if (grafica == 3)
+        {
+            return -10f * x;
+        }else if (grafica == 4)
+        {
+            return 0.01f * (float)Math.Sinh(x);
+        }else if (grafica == 5)
+        {
+            return 0.01f * (float)Math.Cosh(x);
+        }else if (grafica == 6)
+        {
+            return 100f * (float)Math.Sqrt(Math.Abs(x));
+        }else if (grafica == 7){
+            return 0.0001f * ((float)Math.Pow(x, 6) * 0.5f) + (float)Math.Pow(x, 3) + (float)Math.Pow(x, 2) * 0.5f + (2 * x) + 5f;
+        }
+        return 0f;
     }
 
     // Start is called before the first frame update
@@ -47,6 +63,7 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (energiaI - (carga *  (funcion(x) )) < 0)
         {
             x = x - po1x;
@@ -64,4 +81,5 @@ public class Main : MonoBehaviour
         }
         transform.position = new Vector3(po1x, 0, 0);
     }
+
 }
---
+++ Simulador3D/Assets/codigo/Menu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public int graf = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cuadratica()
+    {
+        graf = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void lineal1()
+    {
+        graf = 2;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void lineal2()
+    {
+        graf = 3;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void seno()
+    {
+        graf = 4;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void coseno()
+    {
+        graf = 5;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void raiz()
+    {
+        graf = 6;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void pol()
+    {
+        graf = 7;
+        SceneManager.LoadScene("SampleScene");
+    }
+}
